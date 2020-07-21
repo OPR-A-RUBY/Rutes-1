5841d0137b9929b36600e79cb5d46ee23ccf38f0
OPR-A-RUBin <opr.a.ruby@gmail.com>
2020-07-21 19:11:27 +0400

    Update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,22 @@
 class ArticlesController < ApplicationController
 
     def new
+    	@article = Article.new
     end
     
     def create
+    	@article = Article.new(article_params)
+
+    	if @article.save
+    		redirect_to @article
+    	else
+    		render 'new'
+    	end
+
     end
     
     def index
+    	@articles = Article.all
     end
     
     def show
@@ -20,5 +30,9 @@ class ArticlesController < ApplicationController
     
     def destroy
     end
-	
+
+  private
+  	def article_params
+  		params.require(:article).permit(:title, :body)
+  	end
 end
